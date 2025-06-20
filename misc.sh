aa304f3f4997e88e9abb5e883d3a6b1e987c825d
ianhorn <=>
2025-06-20 12:09:33 -0400

    deploy with history enabled

---
+++ misc.sh
@@ -0,0 +1,7 @@
+npm start -- --open --catalogUrl="https://spved5ihrl.execute-api.us-west-2.amazonaws.com/"
+
+npm run build -- --catalogUrl="https://spved5ihrl.execute-api.us-west-2.amazonaws.com/"
+
+npm run build -- \
+--catalogUrl="https://spved5ihrl.execute-api.us-west-2.amazonaws.com/" \
+--historyMode=hash
