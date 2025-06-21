4d2f428b903905762d27f60da93ecb5710c12b29
ianhorn <=>
2025-06-21 12:44:02 -0400

    update misc

--- misc.sh
+++ misc.sh
@@ -6,6 +6,7 @@ npm run build -- \
 --catalogUrl="https://spved5ihrl.execute-api.us-west-2.amazonaws.com/" \
 --historyMode=hash \
 --pathPrefix="/browser"
+--catalogTitle="KyFromAbove Stac-Browser"
 
 docker run -p 8080:8080 -d --name stac-browser --restart=always \
 -e SB_catalogUrl="http://ec2-52-88-114-187.us-west-2.compute.amazonaws.com" \
