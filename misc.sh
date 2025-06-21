dbe59671bf8e372c055b1730c8d1316c8b317f92
ianhorn <=>
2025-06-20 22:49:00 -0400

    update misc

--- misc.sh
+++ misc.sh
@@ -1,7 +1,12 @@
-npm start -- --open --catalogUrl="https://spved5ihrl.execute-api.us-west-2.amazonaws.com/"
+npm start -- --open --catalogUrl="http://localhost:8082"
 
 npm run build -- --catalogUrl="https://spved5ihrl.execute-api.us-west-2.amazonaws.com/"
 
 npm run build -- \
 --catalogUrl="https://spved5ihrl.execute-api.us-west-2.amazonaws.com/" \
---historyMode=hash
+--historyMode=hash \
+--pathPrefix="/browser"
+
+docker run -p 8080:8080 -d --restart=always \
+-e SB_catalogUrl="http://0.0.0.0:8080" \
+-e SB_catalogTitle="KyFromAbove Stac-Browser" stac-browser:v1
