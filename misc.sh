f6fc462b41b6253aaf6fd79ebf1e52cc38bfd624
ianhorn <=>
2025-06-21 12:59:56 -0400

    add location to conf for browser

--- misc.sh
+++ misc.sh
@@ -2,8 +2,8 @@ npm start -- --open --catalogUrl="http://localhost:8082"
 
 npm run build -- --catalogUrl="https://spved5ihrl.execute-api.us-west-2.amazonaws.com/"
 
-npm run build -- \
---catalogUrl="https://spved5ihrl.execute-api.us-west-2.amazonaws.com/" \
+npm run build \
+--catalogUrl="http://ec2-52-88-114-187.us-west-2.compute.amazonaws.com" \
 --historyMode=hash \
 --pathPrefix="/browser"
 --catalogTitle="KyFromAbove Stac-Browser"
@@ -13,3 +13,8 @@ docker run -p 8080:8080 -d --name stac-browser --restart=always \
 -e SB_catalogTitle="KyFromAbove Stac-Browser" \
 -e SB_pathPrefix="/browser" \
 stac-browser:v1 
+
+
+
+# Copy browser static site to nginx
+docker cp ./dist nginx:/usr/share/nginx/html/browser
