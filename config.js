a21ad0e8ff50461846a9518dc368228b5a5cd727
ianhorn <=>
2025-08-13 23:14:33 -0400

    update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
+// public/config.js
 window.STAC_BROWSER_CONFIG = {
-  catalogUrl: "https://spved5ihrl.execute-api.us-west-2.amazonaws.com/",
-  pathPrefix: "/browser",
-  historyMode: "hash"
+  catalogUrl: "https://spved5ihrl.execute-api.us-west-2.amazonaws.com/stac/",
+  pathPrefix: "/",
+  historyMode: "hash",
 };
